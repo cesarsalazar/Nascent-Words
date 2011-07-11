c4973ade7786e7cdae52934f9397d4a83dba18d5
César Salazar <i.am@cesarsalazar.mx>
2011-07-11 02:10:27 -0700

    Homepage + title editing is more usable

--- public/javascripts/doc.js
+++ public/javascripts/doc.js
@@ -1,37 +1,34 @@
 var typing = false;
+var sound = false;
 var keystrokes = 0;
 
 $(function(){  
   
-  $('h1').hover(function(){
-    $(this).toggleClass('highlight');
-  });
+  $('h1').mouseover(function(){
+    $(this).addClass('highlight');
+  }).mouseout(function(){
+    $(this).removeClass('highlight');
+  })
   
   $('h1').click(function(){
     $(this).after('<input type="text" class="text" id="title" name="title" value="'+$(this).text()+'"/>');
     $(this).hide();
   });
   
+  $('body').click(function(e){
+    if(e.target != $('#title')[0] && e.target != $('h1')[0]){
+      updateTitle();
+    }
+  })
   $('#title').live('keypress', function(event){
     if(event.keyCode == 13){
-      $.ajax({
-        type: 'POST',
-        url: document.location.pathname,
-        data: {title: function(){
-          return $('#title').val();
-        }},
-        success: function(response){
-          $('h1').show().text( $('#title').val() );
-          $('#title').remove();
-        }
-      })
-      
+      updateTitle();
     }
   })
   
-  $('textarea').focus().autogrow().keyup(function(){
+  $('textarea').autogrow().keyup(function(){
     keystrokes += 1;
-    if(typing == false){
+    if(!typing){
       typing = true;
       $('body').removeClass('slacking');
     }
@@ -39,18 +36,18 @@ $(function(){
       saveNewVersion();
     }
   }).keypress(function(){
-    var num = keystrokes;
-    $('#content').append('<audio id="audio'+num+'" src="/audio/keystroke.mp3" preload="auto"/>');
-     document.getElementById('audio'+num).addEventListener('ended', function(){
-       $('#audio'+num).remove();
-     })
-    document.getElementById('audio'+num).play();
-    
-    
-  })
+    if(sound){
+      var num = keystrokes;
+      $('#content').append('<audio id="audio'+num+'" src="/audio/keystroke.mp3" preload="auto"/>');
+       document.getElementById('audio'+num).addEventListener('ended', function(){
+         $('#audio'+num).remove();
+       });
+      document.getElementById('audio'+num).play();
+    };
+  }).focus();
   
   $(document).mousemove(function(){
-    if(typing == true){
+    if(typing){
       typing = false;
       saveNewVersion();
       var wordcount = $('textarea').val().match(/\b/g).length/2;
@@ -74,14 +71,16 @@ var saveNewVersion = function(){
   })
 }
 
-var wordCount = function(s){
-  s = trim(s);
-  return s.split(" ").length
-}
-
-var trim = function(s) {
- s = s.replace(/(^\s*)|(\s*$)/gi,"");
- s = s.replace(/[ ]{2,}/gi," ");
- //s = s.replace(/\n /,"\n ");
- return s;
+var updateTitle = function(){
+  $.ajax({
+    type: 'POST',
+    url: document.location.pathname,
+    data: {title: function(){
+      return $('#title').val();
+    }},
+    success: function(response){
+      $('h1').show().text( $('#title').val() );
+      $('#title').remove();
+    }
+  })
 }
