1b1cee4a0c7145f90f3feb5fb0af9b8d7c53d6e2
César Salazar <i.am@cesarsalazar.mx>
2011-07-16 23:18:29 -0700

    Protecting secrets oops

--- config/init.rb
+++ config/init.rb
@@ -4,9 +4,9 @@ require 'mongo_mapper'
 require 'log_buddy'
 
 # Bitly
-@bitly = YAML.load_file("config/bitly.secret")
-@bitly_user = "" || @bitly["user"]
-@bitly_secret = "" || @bitly["secret"]
+# @bitly = YAML.load_file("config/bitly.secret")
+# @bitly_user = @bitly["user"]
+# @bitly_secret = @bitly["secret"]
 
 # Else read the local configuration
 @config = YAML.load_file("config/database.yaml")
